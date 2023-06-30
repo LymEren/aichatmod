9db39cabb5492f709a895c8ef3f99dc833ae359a
Eyyub Eren <eyyub.eren@etiya.com>
2023-06-30 11:08:05 +0300

    Sample project with pytorch library

---
+++ pyTorch.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import pipeline
+
+# Load the pre-trained model for keyword extraction
+model = pipeline("ner", model="dslim/bert-base-NER", tokenizer="dslim/bert-base-NER")
+
+
+# Function to extract keywords from a given text
+def extract_keywords(text):
+    # Use the pre-trained model to extract named entities (keywords)
+    keywords = model(text)
+
+    # Extract only the entities that represent keywords
+    keywords = [entity['word'] for entity in keywords if entity['entity'] != 'O']
+
+    return keywords
+
+
+# Example usage
+text = "PyTorch is a popular deep learning framework. It provides powerful tools for building and training neural networks."
+keywords = extract_keywords(text)
+
+# Print the extracted keywords
+print(keywords)
